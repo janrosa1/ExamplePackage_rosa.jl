1e43a1730f6e12a2d92fbac62951961c906b8b82
janrosa1 <you@example.comjan.rosa1993@gmail.com>
2020-10-20 19:43:12 +0200

    update tests

--- src/ExamplePackage_rosa.jl
+++ src/ExamplePackage_rosa.jl
@@ -10,11 +10,20 @@ function newtonroot(f,f_prime;x_0,tol = 1E-7, maxiter = 1000)
     x_old=x_0
     normdiff=Inf
     iter=1
+    
+    if(f_prime(x_old)==0)
+        return (value=x_old,normdiff=f(x_old),iter=0)
+    end 
+
     while normdiff>tol && iter<=maxiter
         x_new= x_old - f(x_old)/f_prime(x_old)
+        if isnan(x_new)
+            return nothing
+        end
         normdiff=norm(x_new-x_old)
         x_old=x_new
         iter = iter + 1 
+        
     end
     if(normdiff>tol)
         return nothing
@@ -31,10 +40,18 @@ function newtonroot(f;x_0,tol = 1E-7, maxiter = 1000)
     x_old=x_0
     normdiff=Inf
     iter=1
+    if(f_prime(x_old)==0)
+        return (value=x_old,normdiff=f(x_old),iter=0)
+    end
+
     while normdiff>tol && iter<=maxiter
         x_new= x_old - f(x_old)/f_prime(x_old)
+        if isnan(x_new)
+            return nothing
+        end
         normdiff=norm(x_new-x_old)
         x_old=x_new
+        
         iter += 1 
     end
     if(normdiff>tol)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,39 @@ using ExamplePackage_rosa
 using Test
 
 @testset "ExamplePackage_rosa.jl" begin
-    # Write your tests here.
+    testfn(x)= (x-1)^3
+    testfn_prime(x)=3*(x-1)^2
+    testfn1(x)= exp(x)-1
+    testfn_prime1(x)=exp(x)
+    testfn2(x)= atan(x)
+    testfn_prime2(x)= 1/((x)^2+1) 
+
+    #test for functions with derivative
+
+    root1, conv, iter  = ExamplePackage_rosa.newtonroot(testfn,testfn_prime,x_0=0.0)
+    root2, conv, iter  = ExamplePackage_rosa.newtonroot(testfn1,testfn_prime1,x_0=1.0)
+    root3, conv, iter  = ExamplePackage_rosa.newtonroot(testfn1,testfn_prime1,x_0=-0.3)
+
+    @test abs(root1-1.0)<1e-6
+    @test abs(root2 - 0.0)<1e-6
+    @test abs(root3 - 0.0)<1e-6
+
+    #do the same for functions without derivative
+
+    root1, conv, iter  = ExamplePackage_rosa.newtonroot(testfn,x_0=0.0)
+    root2, conv, iter  = ExamplePackage_rosa.newtonroot(testfn1,x_0=1.0)
+    root3, conv, iter  = ExamplePackage_rosa.newtonroot(testfn1,x_0=-.4)
+
+    @test abs(root1-1.0)<1e-6
+    @test abs(root2 - 0.0)<1e-6
+    @test abs(root3 - 0.0)<1e-6
+
+    #check lack iof convergence
+    
+    test_nc(x) = x^2+1
+
+    answer  = ExamplePackage_rosa.newtonroot(test_nc,x_0=1.0)
+    @test answer==nothing
+
+
 end
